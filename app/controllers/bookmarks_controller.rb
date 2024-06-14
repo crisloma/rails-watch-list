be84fcd5b4dba9d50ac18b089724452299ea52a8
Cristina Loma <crislaragones@gmail.com>
2024-06-14 16:42:56 -0400

    challenge

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,33 +1,27 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only[:new, :create]
-
+  before_action :set_list, only: [:new, :create]
   def new
     @bookmark = Bookmark.new
   end
-
   def create
-    @bookmark = Bbookmark.new(bookmark_params)
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
     else
-      render :new, status: unprocessable_entity # Renderizar 'new' en caso de error
+      render :new, status: :unprocessable_entity # Renderizar 'new' en caso de error
     end
   end
-
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark), notice: 'Bookmark was successfully destroyed.'
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully destroyed.'
   end
-
   private
-
-  def set_list
-    @list = List.find(params[:list_id])
-  end
-
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
